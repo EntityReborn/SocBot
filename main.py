23038dd6abd86bc77b5f24798ec0303b6ca148ad
EntityReborn <entityreborn@gmail.com>
2012-04-03 11:47:26 -0700

    Fixed wrapper for operation on linux systems.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys, os
 from collections import defaultdict
 import logging
--- wrapper.py
+++ wrapper.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 import subprocess
 
 if __name__ == "__main__":
     code = 3
     
     while code == 3:
-        p = subprocess.Popen('python main.py')
+        p = subprocess.Popen('./main.py')
         p.wait()
         code = p.returncode
